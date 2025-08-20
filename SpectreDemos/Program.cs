a6d1ae89beb1e9f62075bf117e4e2fa2f01a6c80
Sergio Di Fiore <difiores@outlook.com>
2025-08-20 16:27:27 -0300

    Add project files.

---
+++ SpectreDemos/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+
+
+
+
+
+// Lesson 2 - Initial setup
+AnsiConsole.MarkupLine("[red bold]Hello World[/]");
+AnsiConsole.MarkupLine("Hello World");
+AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
+
+Console.ReadLine();
+AnsiConsole.Clear();
