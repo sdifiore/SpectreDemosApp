6fa40bd1817c28ad4c803c8c46a5314065b8df0d
Sergio Di Fiore <difiores@outlook.com>
2025-08-26 11:29:43 -0300

    3 - Color and Style

--- SpectreDemos/Program.cs
+++ SpectreDemos/Program.cs
@@ -4,10 +4,33 @@
 
 
 
+
+
+
+
+
+
+
+
+
 // Lesson 2 - Initial setup
-AnsiConsole.MarkupLine("[red bold]Hello World[/]");
-AnsiConsole.MarkupLine("Hello World");
-AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
+//AnsiConsole.MarkupLine("[red bold]Hello World[/]");
+//AnsiConsole.MarkupLine("Hello World");
+//AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
+
+// Lesson 3 - Color and Style
+AnsiConsole.MarkupLine("[red on white bold]Hello[/] [green]World[/]");
+
+Style danger = new Style(
+	foreground: Color.Red,
+	background: Color.White,
+	decoration: Decoration.Bold | Decoration.Italic);
+
+AnsiConsole.Write(new Markup("Danger Text from Style\n", danger));
+
+
+AnsiConsole.Write(new Markup("Danger Text from Style again, no lf", danger));
+AnsiConsole.WriteLine(" and more text on the same line");
 
 Console.ReadLine();
 AnsiConsole.Clear();
